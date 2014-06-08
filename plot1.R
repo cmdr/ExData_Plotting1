e0626f819445f2a93112dcab2a5069d23b7f22a5
cmdr <cmdr@users.noreply.github.com>
2014-06-08 22:48:56 +0200

    Project 1

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ x = as.numeric(data[, 3])
 hist1 = hist(x, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 
 ## Saving plot to file
-png(file = "plot1.png") ## Open png device; create 'plot1.png' in my working directory
+png(file = "plot1.png", width = 480, height = 480, units = "px") ## Open png device; create 'plot1.png' in my working directory
 with(faithful, hist1) ## Create plot and send to a file (no plot appears on screen)
 title(main = "")
 dev.off() ## Close the png file device
--- plot2.R
+++ plot2.R
@@ -9,7 +9,7 @@ x <- c(1:length(x))
 y = as.numeric(data[, 3])
 
 ## Open png device; create 'plot2.png' in my working directory
-png("plot2.png")
+png("plot2.png", width = 480, height = 480, units = "px")
 plot2 = plot(x, y, xlab = "", ylab = "Global Active Power (kilowatts)", xaxt = "n", "l")
 # set the x axis
 axis(1, at = c(1, 1440, 2880), labels = c("Thu", "Fri", "Sat"))
